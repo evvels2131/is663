7dfde5df12fc513a6ac30ddb625f081b774118ec
Tom <tgoralczyk1@gmail.com>
2016-11-11 17:17:47 -0500

    Changes to the tabbar

--- src/customer/js/main.js
+++ src/customer/js/main.js
@@ -11,12 +11,40 @@
   });
 
   // `Home` view
-  myApp.onPageInit('index', function (page) {
-    console.log('home page', page);
+  myApp.onPageInit('index-main', function (page) {
+    console.log('index-main');
+    addActive(0);
   });
 
-  myApp.onPageInit('repairs', function (page) {
-    console.log('repairs', page);
+  // `Repairs` view
+  myApp.onPageInit('repairs-main', function (page) {
+    console.log('repairs-main');
+    addActive(1);
   });
+
+  // `Notifications` view
+  myApp.onPageInit('notifications-main', function (page) {
+    console.log('notifications-main');
+    addActive(2);
+  });
+
+  // `Settings` view
+  myApp.onPageInit('settings-main', function (page) {
+    console.log('settings-main');
+    addActive(3);
+  });
+
+  function addActive(id) {
+    var links = $$('.toolbar-inner').children();
+    var activeLink = links[id];
+
+    $$(links).each(function (idx, value) {
+      if ($$(value).hasClass('active')) {
+        $$(value).removeClass('active');
+      }
+    });
+
+    $$(activeLink).addClass('active');
+  }
 
 })();

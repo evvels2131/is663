9f96bd32f08d340fa2b542f40bc81ee91e46a062
Tom <tgoralczyk1@gmail.com>
2016-11-11 22:21:16 -0500

    Added a second iframe for the service provider prototype

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+
+  $('#customer').click(function () {
+    hide(1);
+  });
+
+  $('#service-provider').click(function () {
+    hide(0);
+  });
+
+  function hide(id) {
+    var iframes = $('.iframes').children();
+    var hide = iframes[id];
+    console.log(iframes);
+
+    $(iframes).each(function (idx, value) {
+      if ($(value).hasClass('hide')) {
+        $(value).removeClass('hide');
+      }
+    });
+
+    $(hide).addClass('hide');
+  }
+
+})();

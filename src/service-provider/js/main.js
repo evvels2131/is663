927b56fcc692c2452fadd980168ef7f322a4b35a
Tom <tgoralczyk1@gmail.com>
2016-11-11 19:38:00 -0500

    Fixed up stuff for service-provider prototype

--- src/service-provider/js/main.js
+++ src/service-provider/js/main.js
@@ -2,11 +2,43 @@
   'use strict';
 
   var myApp = new Framework7();
+
   var $$ = Dom7;
 
-
+  // Add view
   var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
   });
-  
-})
+
+  // `Home` view
+  myApp.onPageInit('index-main', function (page) {
+    console.log('index-main');
+    addActive(0);
+  });
+
+  // `Notifications` view
+  myApp.onPageInit('notifications-main', function (page) {
+    console.log('notifications-main');
+    addActive(1);
+  });
+
+  // `Settings` view
+  myApp.onPageInit('settings-main', function (page) {
+    console.log('settings-main');
+    addActive(2);
+  });
+
+  function addActive(id) {
+    var links = $$('.toolbar-inner').children();
+    var activeLink = links[id];
+
+    $$(links).each(function (idx, value) {
+      if ($$(value).hasClass('active')) {
+        $$(value).removeClass('active');
+      }
+    });
+
+    $$(activeLink).addClass('active');
+  }
+
+})();

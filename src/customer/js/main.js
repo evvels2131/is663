59bc32d4098e3b7dd785e425846bdd5c0ee40a3f
Tom <tgoralczyk1@gmail.com>
2016-11-16 13:04:20 -0500

    Added alerts to the prototype for customer

--- src/customer/js/main.js
+++ src/customer/js/main.js
@@ -34,6 +34,63 @@
     addActive(3);
   });
 
+  // `Schedule Service` view
+  myApp.onPageInit('schedule-service', function (page) {
+    $$('#btn-schedule-service').on('click', function () {
+      myApp.alert('You have successfully requested a service repair.', 'Success!');
+    });
+  });
+
+  // `Contact Us` view
+  myApp.onPageInit('contact-us', function (page) {
+    $$('#btn-contact-us').on('click', function () {
+      myApp.alert('You have successfully sent a message', 'Success!');
+    });
+  });
+
+  // `Make Payment` view
+  myApp.onPageInit('make-payment', function (page) {
+    $$('#btn-make-payment').on('click', function () {
+      myApp.alert('You have successfully submitted a payment.', 'Success!');
+    });
+  });
+
+  // `Contact Service Provider` view
+  myApp.onPageInit('contact-service-provider', function (page) {
+    $$('#btn-contact-service-provider').on('click', function () {
+      myApp.alert('You have successfully contacted the service provider.', 'Success!');
+    });
+  });
+
+  // `Rate` view
+  myApp.onPageInit('rate', function (page) {
+    $$('#btn-rate').on('click', function () {
+      myApp.alert('You have successfully submitted your rating.', 'Success!');
+    });
+  });
+
+  // `General` view
+  myApp.onPageInit('general', function (page) {
+    $$('#btn-general').on('click', function () {
+      myApp.alert('You have successfully submitted your changes.', 'Success!');
+    });
+  });
+
+  // `Help Center` view
+  myApp.onPageInit('help-center', function (page) {
+    $$('#btn-help-center').on('click', function () {
+      myApp.alert('You have successfully contacted the customer service', 'Success!');
+    });
+  });
+
+  // `Report Problem` view
+  myApp.onPageInit('report-problem', function (page) {
+    $$('#btn-report-problem').on('click', function () {
+      myApp.alert('You have successfully reported a problem.', 'Success!');
+    });
+  });
+
+
   function addActive(id) {
     var links = $$('.toolbar-inner').children();
     var activeLink = links[id];

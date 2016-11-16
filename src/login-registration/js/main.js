e98cc6005cedae931da287981491dc0d4297ed3a
Tom <tgoralczyk1@gmail.com>
2016-11-16 10:54:58 -0500

    Added the registration screens

---
+++ src/login-registration/js/main.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  var myApp = new Framework7();
+
+  var $$ = Dom7;
+
+  // Add view
+  var mainView = myApp.addView('.view-main', {
+    dynamicNavbar: true
+  });
+
+})();
